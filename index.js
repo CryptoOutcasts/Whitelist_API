bc6d1c2a4c1add7a4513621d7f81ab8f115e0ff5
unknown <mcdeebt@gmail.com>
2021-10-08 00:20:39 +0300

    updates

--- index.js
+++ index.js
@@ -3,8 +3,12 @@ const app = exp();
 const fs = require("fs")
 const { exit } = require("process");
 
+
+app.use(exp.json());
 let csvToJson = require('convert-csv-to-json');
 const { once } = require("events");
+const e = require("express");
+const { json } = require("express");
 
     let rawdata = fs.readFileSync('config.json');
     let config = JSON.parse(rawdata);
@@ -41,35 +45,33 @@ app.get('/whitelisted/member/:address', (req, res, value) => {
     let sent = false;
     let rawdata = fs.readFileSync('whitelisted.json');
     let members = JSON.parse(rawdata);
-    members.forEach(user => {
-        if(user.member == req.params.address) {
-            if(!sent){
-                res.send([user]);
-                sent = true
-            }
-        }
-    });
-    if(!sent){
-        res.send({"member": undefined, "reserve": undefined})
+    const user = members.find(c => c.member == req.params.address)
+    if(!user){
+        res.status(404).send("the member was not found");
     }
+    res.send(user);
 })
 
-app.put('/whitelisted/update/:address/:reserves_left', (req, res) => {
-    let sent = false;
+app.put('/whitelisted/update/:address', (req, res) => {
+    let index = 0;
+    let result = {}
     let rawdata = fs.readFileSync('whitelisted.json');
     let members = JSON.parse(rawdata);
-    members.forEach(user => {
-        if(user.member == req.params.address) {
-            if(!sent){
-                user.reserve = req.params.reserves_left;
-                res.send([user]);
-                sent = true
-            }
+    members.forEach((whitelisted_user, i) => {
+        if(whitelisted_user.member == req.params.address){
+            result = whitelisted_user
+            index = i;
         }
     });
-    if(!sent){
-        res.send({"member": undefined, "reserve": undefined})
+    if(result == {}){
+        res.status(404).send("User does not exist")
     }
+    result.reserve = req.body.reserve
+    members[index] = result;
+    const data = JSON.stringify(members);
+    fs.writeFile('whitelisted.json', data, 'utf8', (err) =>{ if(err) console.log(err)});
+    res.send(result)
+
 })
 const port = process.env.PORT || 3000
 app.listen(port, ()=>console.log(`listening on port ${port}`));
